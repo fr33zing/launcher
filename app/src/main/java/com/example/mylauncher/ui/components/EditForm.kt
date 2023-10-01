8c865350b939ad5fa9e990415d20123c436e02f1
fr33zing <fr33zing@none.none>
2023-09-30 19:30:53 -0700

    fix keyboard overlap issue

--- app/src/main/java/com/example/mylauncher/ui/components/EditForm.kt
+++ app/src/main/java/com/example/mylauncher/ui/components/EditForm.kt
@@ -1,16 +1,22 @@
 package com.example.mylauncher.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.absolutePadding
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
@@ -18,6 +24,9 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -27,14 +36,20 @@ import com.example.mylauncher.data.NodeKind
 import com.example.mylauncher.data.persistent.Application
 import com.example.mylauncher.data.persistent.Node
 import com.example.mylauncher.data.persistent.Payload
+import com.example.mylauncher.helper.conditional
+import com.example.mylauncher.ui.components.dialog.FuzzyPickerDialog
 import com.example.mylauncher.ui.theme.Background
 import com.example.mylauncher.ui.theme.Foreground
 
 private val extraPadding = 16.dp
 private val spacing = 16.dp
 
+lateinit var enableNormalImePadding: MutableState<Boolean>
+
 @Composable
 fun EditForm(innerPadding: PaddingValues, node: Node, payload: Payload?) {
+    enableNormalImePadding = remember { mutableStateOf(true) }
+
     if (payload != null)
         when (node.kind) {
             NodeKind.Application -> ApplicationEditForm(innerPadding, payload, node)
@@ -46,15 +61,19 @@ fun EditForm(innerPadding: PaddingValues, node: Node, payload: Payload?) {
 @Composable
 private fun EditFormColumn(
     innerPadding: PaddingValues,
-    modifier: Modifier = Modifier,
     content: @Composable (ColumnScope.() -> Unit)
-) =
-    Column(
-        modifier =
-            Modifier.padding(innerPadding).padding(extraPadding).fillMaxWidth().then(modifier),
-        verticalArrangement = Arrangement.spacedBy(spacing),
-        content = content
-    )
+) {
+    val scrollState = rememberScrollState()
+    Box(Modifier.conditional(enableNormalImePadding.value) { imePadding() }.fillMaxHeight()) {
+        Box(Modifier.fillMaxSize().verticalScroll(scrollState).height(IntrinsicSize.Max)) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(spacing),
+                modifier = Modifier.padding(innerPadding).padding(extraPadding).fillMaxHeight(),
+                content = content
+            )
+        }
+    }
+}
 
 @Composable
 private fun DefaultEditForm(innerPadding: PaddingValues, node: Node) {
@@ -67,26 +86,27 @@ private fun ApplicationEditForm(
     payload: Payload?,
     node: Node,
 ) {
+    val configuration = LocalConfiguration.current
+    val density = LocalDensity.current
+
     val application = payload as Application
 
-    val (height, width) = LocalConfiguration.current.run { screenHeightDp.dp to screenWidthDp.dp }
-    val screenMin = if (width < height) width else height
-    val buttonOuterPadding = screenMin * 0.15f
-    val buttonIconSize = screenMin * 0.215f
-    val buttonIconTextSpacing = screenMin * -0.02f
-    val buttonFontSizeDp = screenMin * 0.045f
-    val buttonFontSize = with(LocalDensity.current) { buttonFontSizeDp.toSp() }
 
-    EditFormColumn(innerPadding, modifier = Modifier.fillMaxSize()) {
+    EditFormColumn(innerPadding) {
         Column(
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier =
-                Modifier.fillMaxWidth()
-                    .weight(1f)
-                    .padding(buttonOuterPadding)
-                    .absolutePadding(bottom = extraPadding)
+            modifier = Modifier.weight(1f).fillMaxWidth()
         ) {
+            val (height, width) =
+                remember { configuration.run { screenHeightDp.dp to screenWidthDp.dp } }
+            val screenMin = remember { if (width < height) width else height }
+            val buttonSize = remember { screenMin * 0.4f }
+            val buttonIconSize = remember { screenMin * 0.215f }
+            val buttonIconTextSpacing = remember { screenMin * -0.02f }
+            val buttonFontSizeDp = remember { screenMin * 0.045f }
+            val buttonFontSize = remember { with(density) { buttonFontSizeDp.toSp() } }
+
             Button(
                 onClick = { /*TODO*/},
                 shape = CircleShape,
@@ -95,7 +115,7 @@ private fun ApplicationEditForm(
                         containerColor = Foreground,
                         contentColor = Background
                     ),
-                modifier = Modifier.aspectRatio(1f)
+                modifier = Modifier.requiredSize(buttonSize)
             ) {
                 Column(
                     verticalArrangement = Arrangement.spacedBy(buttonIconTextSpacing),
