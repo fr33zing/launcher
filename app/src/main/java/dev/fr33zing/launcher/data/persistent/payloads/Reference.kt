3555ca9fd8db84d04ffa001f7ba8b2a45d7ac142
fr33zing <fr33zing@none.none>
2023-10-11 15:54:23 -0700

    make reference payload activation function work

--- app/src/main/java/dev/fr33zing/launcher/data/persistent/payloads/Reference.kt
+++ app/src/main/java/dev/fr33zing/launcher/data/persistent/payloads/Reference.kt
@@ -1,7 +1,23 @@
 package dev.fr33zing.launcher.data.persistent.payloads
 
+import android.content.Context
 import androidx.room.Entity
+import dev.fr33zing.launcher.data.persistent.AppDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Entity
 class Reference(payloadId: Int, nodeId: Int, var targetId: Int? = null) :
-    Payload(payloadId, nodeId)
+    Payload(payloadId, nodeId) {
+
+    override fun activate(db: AppDatabase, context: Context) {
+        if (targetId == null) return
+
+        CoroutineScope(Dispatchers.IO).launch {
+            db.nodeDao().getNodeById(targetId!!)?.let { target ->
+                db.getPayloadByNodeId(target.kind, target.nodeId)?.activate(db, context)
+            }
+        }
+    }
+}
