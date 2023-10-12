7a0c8aedb87cacab089c900038d855f4225c7a91
freezing <freezing@freezing.(none)>
2023-10-12 14:04:34 -0700

    fix rootProject.name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,5 +14,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "My Launcher"
+rootProject.name = "launcher"
+
 include(":app")
