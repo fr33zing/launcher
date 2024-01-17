ab1c9a81309a9d5576593dad44e0d6b95a439477
fr33zing <fr33zing@none.none>
2024-01-16 18:46:05 -0800

    add some exceptions

--- app/src/main/java/dev/fr33zing/launcher/data/utility/Exceptions.kt
+++ app/src/main/java/dev/fr33zing/launcher/data/utility/Exceptions.kt
@@ -2,7 +2,19 @@ package dev.fr33zing.launcher.data.utility
 
 import dev.fr33zing.launcher.data.persistent.Node
 
+class NullNodeException() : Exception("Node is null.")
+
+fun Node?.notNull(): Node = this ?: throw NullNodeException()
+
 class NullPayloadException(val node: Node) : Exception("Node ${node.nodeId} payload is null.")
 
 class PayloadClassMismatchException(val node: Node) :
     Exception("Node ${node.nodeId} kind is ${node.kind} but its payload class does not match.")
+
+class UnreachableException(unreachableReason: String) :
+    Exception(
+        "Code that was meant to be unreachable was executed. Unreachable reason: $unreachableReason"
+    )
+
+fun unreachable(unreachableReason: () -> String): Nothing =
+    throw UnreachableException(unreachableReason())
